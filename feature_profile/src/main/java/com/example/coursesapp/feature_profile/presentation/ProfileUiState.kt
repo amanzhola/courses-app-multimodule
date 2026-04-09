381f5d436498c8aaa711708f3a0fe55062d69655
Ecballium elaterium <amanzholaimov@gmail.com>
2026-04-09 21:44:35 +0500

    Ignore IDE gradle config and fix ProfileUiState import

--- feature_profile/src/main/java/com/example/coursesapp/feature_profile/presentation/ProfileUiState.kt
+++ feature_profile/src/main/java/com/example/coursesapp/feature_profile/presentation/ProfileUiState.kt
@@ -1,6 +1,7 @@
 package com.example.coursesapp.feature_profile.presentation
 
 import com.example.coursesapp.core.ui.UiText
+import com.example.coursesapp.feature_profile.presentation.adapter.ProfileCourseUi
 
 data class ProfileUiState(
     val isLoading: Boolean = false,
